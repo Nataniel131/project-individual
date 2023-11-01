9d213141cea4a8b19441a6e7b406a3090baf7abc
Nataniel Rutkowski <rutkowski.nataniel@gmail.com>
2023-11-01 18:37:41 +0100

    Add class Products and object app for script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,63 @@
 import { products } from './data.js';
+import { templates } from './setting.js';
 
-const tplHelloSource = document.querySelector('#template-product').innerHTML;
-const tplHello = Handlebars.compile(tplHelloSource);
-const targetElement = document.querySelector('.products-list');
+class Products {
+  constructor() {
+    const thisProduct = this;
 
-for (const product of products) {
-  targetElement.innerHTML += tplHello(product);
+    thisProduct.getElements();
+    thisProduct.renderProducts();
+  }
+
+  getElements() {
+    const thisProduct = this;
+
+    thisProduct.dom = {};
+    thisProduct.dom.productList = document.querySelector('.products-list');
+  }
+
+  renderProducts() {
+    const thisProduct = this;
+
+    for (const product of products) {
+      thisProduct.dom.productList.innerHTML += templates.cartProduct(product);
+    }
+  }
 }
 
-const app {};
+const app = {
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector('#pages').children;
+    thisApp.navLinks = document.querySelectorAll('.navigation-pages a');
+    
+    thisApp.activatePages(thisApp.navLinks[0].getAttribute('href').replace('#', ''));
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePages(id);
+      });
+    }
+  },
+
+  activatePages: function(pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle('active', page.id == pageId || pageId == 'home' && page.id != 'contact');
+    }
+  },
+
+  init: function() {
+    new Products();
+    app.initPages();
+  },
+};
+
+app.init();
